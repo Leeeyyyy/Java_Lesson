d576d883565963256977a9b7a6b3788ff2f73cd2
Leeeyyyy <noeltuazoncasas1903@gmail.com>
2026-05-29 07:58:51 +0000

    Day 14: ForLoopsLesson

--- chapter2/LoopsLesson.java
+++ chapter2/LoopsLesson.java
@@ -1,5 +1,7 @@
 package chapter2;
 
+import java.util.Scanner;
+
 public class LoopsLesson {
     public static void main(String[] args) {
         byte a = 1;
@@ -11,5 +13,20 @@ public class LoopsLesson {
 
         System.out.println("end of program");
 
+        for (int b = 0; b<10; b++)
+            System.out.println("b = "+b);
+        Scanner q = new Scanner(System.in);
+        int limit = q.nextInt();
+        for(int x = 1; x<limit; x++){
+            for(int y = 1; y<=x; y++){
+        System.out.print(x);
+             }
+        System.out.println();
+         }
+           //for each 
+           String[] colors = {"black", "red", "yellow", "white", "blue"};
+           for (String color: colors)
+            System.out.println("Color: " + color);
     }
+  
 }

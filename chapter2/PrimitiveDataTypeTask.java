29ee909cac8873151e34bf55524c67340a3e3b9f
Leeeyyyy <noeltuazoncasas1903@gmail.com>
2026-05-30 14:35:53 +0800

    PrimitiveDataTYpeTask

---
+++ chapter2/PrimitiveDataTypeTask.java
@@ -0,0 +1,20 @@
+package chapter2;
+
+public class PrimitiveDataTypeTask {
+
+    public static void main(String[] args) {
+        
+        char h = 'H';
+        int number = 33110;
+        char w = 'w';
+        int zero = 0;
+        char r = 'r';
+        int one = 1;
+        char d = 'd';
+        char space = ' ';
+        boolean status = true;
+
+        System.out.println("" + h + number + space+  w + zero + r + one + d + space + status);
+    }
+    
+}

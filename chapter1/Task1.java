3f8dd63d2354694e6b18de7ab032ab552171f4b3
Noel Casas <leeey1903@gmail.com>
2026-06-05 23:15:21 +0800

    Task1

---
+++ chapter1/Task1.java
@@ -0,0 +1,16 @@
+package chapter1;
+//Task Sheet 1-1-3
+public class Task1 {
+
+    public static void main(String[] args) {
+        int check_number = 10;
+        String message;
+
+        for (int i = 1; i <= check_number; i++) {
+            message = (i % 2 == 0)
+                ? i + " is even number"
+                : i + " is odd number";
+            System.out.println(message);
+        }
+    }
+}
